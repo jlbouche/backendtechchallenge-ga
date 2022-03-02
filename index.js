789c4717218fa99f2ddac803e3a9c0170619f103
jlbouche <jennalbouche@gmail.com>
2022-03-02 10:48:44 -0800

    complete teamid and companyid get requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const express = require('express')
-const path = require("path")
+const path = require("path");
+const req = require('express/lib/request');
 
 const app = express();
 const port = process.env.PORT || "3000"
@@ -22,3 +23,25 @@ function allAttendees(req, res){
 }
 
 app.get("/", (allAttendees))
+
+app.get('/team/:id', (req, res) => {
+    let id = req.params.id
+    let teamArr = []
+    attendees.forEach(elem => {
+        if (elem.team == id){
+            teamArr.push(elem.name)
+        }
+    })
+    res.send(`${teamArr}`)
+});
+
+app.get('/company/:id', (req, res) => {
+    let id = req.params.id
+    let companyArr = []
+    attendees.forEach(elem => {
+        if (elem.Company == id){
+            companyArr.push(elem.name)
+        }
+    })
+    res.send(`${companyArr}`)
+})
