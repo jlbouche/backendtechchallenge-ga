cceda899bfe558c9b9553f9ee49597ee76195e10
jlbouche <jennalbouche@gmail.com>
2022-03-03 14:25:13 -0800

    add company view route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-let express = require('express')
+let express = require('express');
 let router = express.Router();
 let attendeeCtrl = require('../controllers/attendees')
 
@@ -6,6 +6,7 @@ router.get('/', attendeeCtrl.index);
 router.get('/new', attendeeCtrl.new);
 router.get('/:id', attendeeCtrl.attendeeView);
 router.get('/team/:id', attendeeCtrl.teamView);
+router.get('/company/:id', attendeeCtrl.companyView)
 router.post('/', attendeeCtrl.create);
 
 module.exports = router;
